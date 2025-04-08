253b66c0522d57d101cf4db5dd3f2f236e74b81d
Faareh-Ahmed <faarehahmed@gmail.com>
2025-04-09 00:30:03 +0500

    Clerk Webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,26 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher  } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+
+
+// Mark /api/webhooks and any subroutes as public
+const isPublicRoute = createRouteMatcher([
+  "/api/webhooks/clerk",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  // Make webhook routes public
+  if (isPublicRoute(req)) {
+    const authObject = await auth();
+    return new Response(null, { status: 200 }); // Allow public access
+  }
+
+  // All other routes are protected (authenticated)
+  const authObject = await auth();
+  if (!authObject.userId) {
+    return new Response("Unauthorized", { status: 401 }); // Protect route
+  }
+  return new Response(null, { status: 200 }); // Allow access for authenticated users
+});
 
 export const config = {
   matcher: [
