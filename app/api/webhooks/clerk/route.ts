fbe84d4ecb033084617f7548451c700a3ced28da
Faareh-Ahmed <faarehahmed@gmail.com>
2025-04-09 00:57:50 +0500

    Webhooks Clerk

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -18,18 +18,7 @@ export async function POST(req: Request) {
     );
   }
 
-  // Get the headers
-  const headerPayload = await headers();
-  const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
-  const svix_signature = headerPayload.get("svix-signature");
-
-  // If there are no headers, error out
-  if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
-      status: 400,
-    });
-  }
+ 
 
   // Get the body
   const payload = await req.json();
@@ -38,6 +27,20 @@ export async function POST(req: Request) {
   // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
+
+   // Get the headers
+   const headerPayload = await headers();
+   const svix_id = headerPayload.get("svix-id");
+   const svix_timestamp = headerPayload.get("svix-timestamp");
+   const svix_signature = headerPayload.get("svix-signature");
+ 
+   // If there are no headers, error out
+   if (!svix_id || !svix_timestamp || !svix_signature) {
+     return new Response("Error occured -- no svix headers", {
+       status: 400,
+     });
+   }
+   
   let evt: WebhookEvent;
 
   // Verify the payload with the headers
